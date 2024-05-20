bb727fd4476f6029265d3402e36a4f7eec612562
Lilia Allen Rowland <lillyallenrowland@gmail.com>
2024-05-20 14:00:00 -0400

    Add Validator agent

---
+++ src/agents/validator.py
@@ -0,0 +1,10 @@
+"""Validator agent."""
+from .base import BaseAgent
+
+
+class ValidatorAgent(BaseAgent):
+    """Validates responses."""
+    
+    @property
+    def name(self) -> str:
+        return "Validator"
